ce7ac035f0c3fe290bdb8da0b2e1f06bcdb0ed0d
EduardoTello <tellodc@gmail.com>
2014-11-22 00:33:58 -0600

    Cambio Vario a Algoritmo Genético
    Ya regresa una lista de acciones que suman la mayor cantidad de puntos
    PERO aun le falta meterle todas las restricciones, esto es
    precondiciones, que no se puedan repetir  acciones, acciones que a
    fuerza van encadenadas, etc.. Esto es: aun le falta un chorro de chamba
    pero ya es tarde y tengo sueño, le sigo un rato mañana.

--- AlgoritmoGenetico.pl
+++ AlgoritmoGenetico.pl
@@ -3,36 +3,62 @@
 %%  Algoritmo Genetico para generar soluciones al problema del robot de servicio.
 
 %% Esto Establece la generación aleatoria inicial y llama la parte cíclica del algoritmo.
-algoritmoGenetico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,MejorIndividuo):-
+algoritmoGenetico(NumeroDeIndividuos,Tamano,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,MejorIndividuo):-
+													regresaTodaLaActividad(ListaDeActividadesPosibles),
+													length(ListaDeActividadesPosibles,NumeroDeAcciones),
 													generaPoblacionAleatoria(NumeroDeIndividuos,Tamano,NumeroDeAcciones,Poblacion),
-													cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,Poblacion,MejorIndividuo),
+													cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,Poblacion,ListaDeActividadesPosibles,MejorIndividuoSinTraducir),
+													MejorIndividuoSinTraducir = [Fitness|Acciones],
+													traduceIndividuo(Acciones,ListaDeActividadesPosibles,MejorIndividuoSinFitness),
+													MejorIndividuo = [Fitness|MejorIndividuoSinFitness],
 													!.
 
+traduceIndividuo([],ListaDeActividadesPosibles,IndividuoTraducido):-
+													IndividuoTraducido = [],
+													!.
+traduceIndividuo([Siguiente|Resto],ListaDeActividadesPosibles,IndividuoTraducido):-
+													traduceAccion(Siguiente,ListaDeActividadesPosibles,AccionTraducida),
+													traduceIndividuo(Resto,ListaDeActividadesPosibles,IndividuoTraducidoIncompleto),
+													append([AccionTraducida],IndividuoTraducidoIncompleto,IndividuoTraducido),
+													!.
+
+traduceAccion(NumeroDeActividad,[],AccionTraducida):-
+													AccionTraducida = [],
+													!.
+traduceAccion(NumeroDeActividad,[SiguienteAccitividad|RestoDeActividades],AccionTraducida):-
+													SiguienteAccitividad = [NumeroDeActividad|Actividad],
+													Actividad = [A,_,D,_,_,_],
+													AccionTraducida = [A,D],
+													!
+													;
+													traduceAccion(NumeroDeActividad,RestoDeActividades,AccionTraducida),
+													!.									
+
 %% Esto es el ciclo básico del algoritmo genético.
-cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,0,Poblacion,MejorIndividuo):-
+cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,0,Poblacion,ListaDeActividadesPosibles,MejorIndividuo):-
 													MejorIndividuo = [0,0],
 													!.
-cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,Poblacion,MejorIndividuo):-
+cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generaciones,Poblacion,ListaDeActividadesPosibles,MejorIndividuo):-
 													Generaciones > 0,
-													evaluaPoblacion(Poblacion,PoblacionEvaluada),
+													evaluaPoblacion(Poblacion,ListaDeActividadesPosibles,PoblacionEvaluada),
 													ordenaPoblacion(PoblacionEvaluada,PoblacionOrdenada),
 													PoblacionOrdenada = [MejorIndividuoActual|ElResto],
 													cruzaPoblacion(PoblacionOrdenada,ProbabilidadDeCruzamiento,PoblacionCruzada),
 													mutaPoblacion(PoblacionCruzada,ProbabilidadDeMutacion,NumeroDeAcciones,PoblacionMutada),
 													%% Hacemos estoporque estamos usando elitismo total.
 													append(PoblacionOrdenada,PoblacionMutada,PoblacionDoble),
-													evaluaPoblacion(PoblacionDoble,PoblacionDobleEvaluada),
+													evaluaPoblacion(PoblacionDoble,ListaDeActividadesPosibles,PoblacionDobleEvaluada),
 													ordenaPoblacion(PoblacionDobleEvaluada,PoblacionDobleOrdenada),
 													%% Aqui regresamos a la población del tamaño original con los mejores individuos.
 													split_at(NumeroDeIndividuos,PoblacionDobleOrdenada,PoblacionSuperior,PoblacionInferior),
 													Generacioncitas is Generaciones - 1,
-													cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generacioncitas,PoblacionSuperior,MejorIndividuoPreliminar),
+													cicloBasico(NumeroDeIndividuos,Tamano,NumeroDeAcciones,ProbabilidadDeCruzamiento,ProbabilidadDeMutacion,Generacioncitas,PoblacionSuperior,ListaDeActividadesPosibles,MejorIndividuoPreliminar),
 													mejor(MejorIndividuoActual,MejorIndividuoPreliminar,MejorIndividuo),
-													write('Generacion: '),
-													write(Generaciones),
-													write(', Mejor Individuo: '),
-													write(MejorIndividuoActual),
-													nl,
+													% write('Generacion: '),
+													% write(Generaciones),
+													% write(', Mejor Individuo: '),
+													% write(MejorIndividuoActual),
+													% nl,
 													!.
 
 %% Esto regresa el individuo que tiene el fitness más grande.
@@ -74,23 +100,35 @@ generaUNindividuoAleatorio(Tamano,NumeroDeAcciones,Individuo):-
 													!.
 
 %% Esto saca el fitness de la población entera.
-evaluaPoblacion([],PoblacionEvaluada):-
+evaluaPoblacion([],ListaDeActividadesPosibles,PoblacionEvaluada):-
 													PoblacionEvaluada = [],
 													!.
-evaluaPoblacion([PrimerIndividuo|RestoDeIndividuos],PoblacionEvaluada):-
-													evaluaPoblacion(RestoDeIndividuos,PoblacionEvaluadaIncompleta),
+evaluaPoblacion([PrimerIndividuo|RestoDeIndividuos],ListaDeActividadesPosibles,PoblacionEvaluada):-
+													evaluaPoblacion(RestoDeIndividuos,ListaDeActividadesPosibles,PoblacionEvaluadaIncompleta),
 													PrimerIndividuo = [Fitness|Genes],
-													evaluaIndividuo(Genes,Fitness2),
+													evaluaIndividuo(Genes,ListaDeActividadesPosibles,Fitness2),
 													append(PoblacionEvaluadaIncompleta,[[Fitness2|Genes]],PoblacionEvaluada),
 													!.
 
 %% Esto le saca el fitness a un individuo solo.
-evaluaIndividuo([],Fitness):- 
+evaluaIndividuo([],ListaDeActividadesPosibles,Fitness):- 
 													Fitness is 0,
 													!.
-evaluaIndividuo([Cabeza|Cola],Fitness):-
-													evaluaIndividuo(Cola,FitnessIncompleto),
-													Fitness is Cabeza + FitnessIncompleto,
+evaluaIndividuo([Cabeza|Cola],ListaDeActividadesPosibles,Fitness):-
+													regresaRecompensa(Cabeza,ListaDeActividadesPosibles,Recompensa),
+													evaluaIndividuo(Cola,ListaDeActividadesPosibles,FitnessIncompleto),
+													Fitness is Recompensa + FitnessIncompleto,
+													!.
+
+regresaRecompensa(NumeroDeActividad,[],Recompensa):- 
+													Recompensa = 0,
+													!.
+regresaRecompensa(NumeroDeActividad,[SiguienteAccitividad|RestoDeActividades],Recompensa):-								
+													SiguienteAccitividad = [NumeroDeActividad|Actividad],
+													Actividad = [_,_,_,_,_,Recompensa],
+													!
+													;
+													regresaRecompensa(NumeroDeActividad,RestoDeActividades,Recompensa),
 													!.
 
 %% Esto ordena de mayor a menor una población según el fitness (primer entrada) de cada individuo.
@@ -158,6 +196,118 @@ mutaIndividuos(ElPrimero,ProbabilidadDeMutacion,NumeroDeAcciones,ElPrimeroMutado
 													ElPrimero = ElPrimeroMutado,
 													!.
 
+% Regresa toda la actividad posible del robot
+regresaTodaLaActividad(Y):-
+													regresaTodasLasAccionesInicio(Y1),
+													regresaTodosLosMovimientosInicio(Y2),
+													append(Y1,Y2,Y3),
+													numeraLasActividades(1,Y3,Y),
+													!.
+
+numeraLasActividades(_,[],Y):-					
+													Y = [],
+													!.
+numeraLasActividades(Indice,[X|Xs],Y):-			
+													X = [H|T],
+													Ys = [Indice|T],
+													Siguiente is Indice + 1,	
+													numeraLasActividades(Siguiente,Xs,Yt),
+													append([Ys],Yt,Y),
+													!.
+
+% Regresa todas la acciones posibles del robot
+regresaTodasLasAccionesInicio(Y):-
+													extensionDeUnaClaseInicio('cosas',X),
+													regresaTodasLasAcciones(X,Y),
+													!.
+
+regresaTodasLasAcciones([],Y):- 
+													Y = [],
+													!.
+regresaTodasLasAcciones([X|Xs],Y):-
+													regresaAccionesDeUnaCosa(X,Ys),
+													regresaTodasLasAcciones(Xs,Yt),
+													append(Ys,Yt,Y), 
+													!.
+
+regresaAccionesDeUnaCosa(X,Y):-
+													regresaTuplaPorNombreInicio(X,Z),
+													Z = [_,_,_,L],
+													L = [Cabeza|Cola],
+													segundoTermino(Cabeza,Ub),
+													regresaTuplaPorIdInicio(Ub,P),
+													regresaNombre(P,Ubicacion),
+													regresaAcciones(Z,M),
+													M = [H,T],
+													segundoTermino(T,B),
+													tablaAcciones(X,B,Ubicacion,Y),
+													!.
+
+regresaAcciones(X,Y):-
+													X=[_,_,Y|_],
+													!.
+
+tablaAcciones(Dato,[],Ubicacion,Y):- 
+													Y=[],
+													!.
+tablaAcciones(Dato,[X|Xs],Ubicacion,Y):-
+													X = [X1,X2,X3,X4],
+													segundoTermino(X1,Accion),
+													segundoTermino(X2,Probadilidad),
+													segundoTermino(X3,Costo),
+													segundoTermino(X4,Recompensa),
+													T = [0,Accion,Ubicacion,Dato,Probadilidad,Costo,Recompensa],
+													tablaAcciones(Dato,Xs,Ubicacion,Yi),
+													append([T],Yi,Y),
+													!.
+
+% Regresa todos los movimientos posibles del robot
+regresaTodosLosMovimientosInicio(Y):-
+													extensionDeUnaClaseInicio('lugares',X),
+													regresaTodosLosMovimientos(X,Y),
+													!.
+
+
+regresaTodosLosMovimientos([],Y):- 
+													Y = [],
+													!.
+regresaTodosLosMovimientos([X|Xs],Y):-
+													regresaMovimientosDeUnLugar(X,Ys),
+													regresaTodosLosMovimientos(Xs,Yt),
+													append(Ys,Yt,Y), 
+													!.
+
+% Regresa los movimientos de un lugar
+regresaMovimientosDeUnLugar(X,Y):-
+													regresaTuplaPorNombreInicio(X,Z),
+													regresaMovimientos(Z,M),
+													M=[H|T],
+													H=[C|V],
+													segundoTermino(C,B),
+													tablaMovimientos(X,B,Y),
+													!.
+
+regresaMovimientos(X,Y):-
+													X=[_,_,_|Y],
+													!.
+
+% Regresa Tabla de movimientos de un lugar
+tablaMovimientos(Dato,[],Y):- 
+													Y=[],
+													!.
+tablaMovimientos(Dato,[X|Xs],Y):-
+													X = [X1,X2,X3,X4],
+													segundoTermino(X1,Clave),
+													regresaTuplaPorIdInicio(Clave,K),
+													regresaNombre(K,Nombre),
+													segundoTermino(X2,Probadilidad),
+													segundoTermino(X3,Costo),
+													segundoTermino(X4,Recompensa),
+													T = [0,'movimiento',Dato,Nombre,Probadilidad,Costo,Recompensa],
+													tablaMovimientos(Dato,Xs,Yi),
+													append([T],Yi,Y),
+													!.
+
 %% Función split_at de hprolog.pl (me gustaría poder cargarlo pero no encuentro como).
 split_at(0,L,[],L) :- !.
 split_at(N,[H|T],[H|L1],L2) :-
@@ -168,3 +318,281 @@ split_at(N,[H|T],[H|L1],L2) :-
 ho(hom).
 ho(bar).
 h(Y):- ho(Y).
+
+% Inicialización.
+:- initialization(main).
+main:- consult('main.pl'),consult('consultas.pl').
+
+% Operador de la Base del Dato.
+:- op(15, xfx, '=>').
+=>(X,Y).
+
+% Regresan partes de la tupla.
+regresaId([H|T],Y):- valor(id,[H],Y).
+regresaId_padre([H|T],Y):- valor(id_padre,T,Y).
+regresaPropiedades([H|T],Y):- T=[HT|TT], 
+								TT = [Y|TTT].
+regresaRelaciones([H|T],Y):- T=[HT|TT], 
+								TT = [HTT|Y].
+
+% Regresa el nombre asociado a una tupla.
+regresaNombre(X,Y):- 
+									regresaPropiedades(X,S), 
+									valor(nombre,S,Y).
+
+% Regresa el nombre mas el operador asociado a una tupla.
+regresaNombreCompleto(X,Y):- 
+									regresaPropiedades(X,S), 
+									S=[H|T], Y = H.
+
+% Regresa una tupla entera de la base de datos según un nombre.
+regresaTuplaPorNombreInicio(X,Y):- 
+									rb(W), 
+									regresaTuplaPorNombre(X,W,Y),!.
+regresaTuplaPorNombre(X,[],Y):-fail.
+regresaTuplaPorNombre(X,[H|T],Y):- 
+									regresaNombre(H,S), 
+									X==S, 
+									Y = H, !
+									; 
+									regresaTuplaPorNombre(X,T,Y), !.
+
+% Regresa una tupla entera de la base de datos según un id.
+regresaTuplaPorIdInicio(X,Y):- 
+									rb(W), 
+									regresaTuplaPorId(X,W,Y),!.
+regresaTuplaPorId(X,[],Y):-fail.
+regresaTuplaPorId(X,[H|T],Y):- 
+									regresaId(H,S), 
+									X==S, 
+									Y = H, !
+									; 
+									regresaTuplaPorId(X,T,Y), !. 
+
+% Regresan ya sea el lado izquierdo o derecho del operador (siempre y cuando estos sean átomos).
+primerTermino(X,Y) :- X = =>(Y,_).
+segundoTermino(X,Y):- X = =>(_,Y).
+
+% Busca en una lista de atributos si alguno coincide con el pedido y regresa su valor en caso de que sea así.
+valor(_,[],_):- fail.
+valor(X,[H|_],Y):- primerTermino(H,X), segundoTermino(H,Y), !.
+valor(X,[H|T],Y):- primerTermino(H,Z), X\=Z, valor(X,T,Y).
+
+cabeza([H|_],Y):- Y=H.
+cabeza(X,Y):- Y=X.
+cola([_|T],Y):- Y=T.
+cola(X,Y):- Y=X.
+
+% Regresa todos los objetos hijos directos o indirectos de una clase dada.
+extensionDeUnaClaseInicio(X,Y):- 
+									rb(W), 
+									regresaTuplaPorNombre(X,W,S), 
+									regresaId(S,L), 
+									extensionDeUnaClase(L,W,Y),!.
+extensionDeUnaClase(X,[],Y):- Y = [].
+extensionDeUnaClase(X,[H|T],Y):- 
+									regresaId_padre(H,S), 
+									X==S,
+									regresaNombre(H,N),
+									regresaId(H,I), 
+									string_chars(I,J), 
+									J=[C|V], 
+									C == 'c',
+									extensionDeUnaClaseInicio(N,P),
+									extensionDeUnaClase(X,T,R), 
+									append(P,R,Y),!
+									;
+									regresaId_padre(H,S), 
+									X==S,
+									regresaNombre(H,N),
+									regresaId(H,I), 
+									string_chars(I,J), 
+									J=[C|V], 
+									C == 'o',
+									extensionDeUnaClase(X,T,R), 
+									append([N],R,Y),!
+									; 
+									extensionDeUnaClase(X,T,Y),!.
+
+% Contiene la base (después la cambiaré para que la base la saque de un archivo)
+rb(Y):- Y = [
+	[
+		id=>c1,
+		id_padre=>c0,
+		[nombre=>robot],
+		[ubicacion=>o5]
+	],
+	[
+		id=>o2,
+		id_padre=>c1,
+		[nombre=>brazoIzquierdo],
+		[]
+	],
+	[
+		id=>o3,
+		id_padre=>c1,
+		[nombre=>brazoDerecho],
+		[]
+	],
+	[
+		id=>c4,
+		id_padre=>c0,
+		[nombre=>lugares],
+		[]
+	],
+	[
+		id=>o5,
+		id_padre=>c4,
+		[nombre=>inicio],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>1.00,costo=>0.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>0.98,costo=>6.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>0.98,costo=>8.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>0.98,costo=>9.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>0.98,costo=>10.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>0.98,costo=>9.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>o6,
+		id_padre=>c4,
+		[nombre=>estante1],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>0.98,costo=>6.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>1.00,costo=>0.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>0.97,costo=>12.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>0.97,costo=>15.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>0.96,costo=>16.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>0.97,costo=>12.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>o7,
+		id_padre=>c4,
+		[nombre=>estante2],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>0.98,costo=>8.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>0.97,costo=>12.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>1.00,costo=>0.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>0.98,costo=>6.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>0.98,costo=>8.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>0.96,costo=>16.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>o8,
+		id_padre=>c4,
+		[nombre=>mesa1],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>0.98,costo=>9.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>0.97,costo=>15.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>0.98,costo=>6.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>1.00,costo=>0.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>0.99,costo=>2.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>0.97,costo=>13.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>o9,
+		id_padre=>c4,
+		[nombre=>mesa2],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>0.98,costo=>10.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>0.96,costo=>16.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>0.98,costo=>8.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>0.99,costo=>2.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>1.00,costo=>0.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>0.97,costo=>12.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>o10,
+		id_padre=>c4,
+		[nombre=>mesa3],
+		[movimiento=>[
+			[lugar=>o5,probabilidad=>0.98,costo=>9.0,recompensa=>0.0],
+			[lugar=>o6,probabilidad=>0.97,costo=>12.0,recompensa=>0.0],
+			[lugar=>o7,probabilidad=>0.96,costo=>16.0,recompensa=>0.0],
+			[lugar=>o8,probabilidad=>0.97,costo=>13.0,recompensa=>0.0],
+			[lugar=>o9,probabilidad=>0.97,costo=>12.0,recompensa=>0.0],
+			[lugar=>o10,probabilidad=>1.00,costo=>0.0,recompensa=>0.0]             
+			]
+		]
+	],
+	[
+		id=>c11,
+		id_padre=>c0,
+		[nombre=>cosas],
+		[]
+	],
+	[
+		id=>o12,
+		id_padre=>c11,
+		[
+			nombre=>hamburguesa,
+			acciones=>[
+				[nombre=>buscar,probabilidad=>0.95,costo=>5.0,recompensa=>50.0],
+				[nombre=>agarrar,probabilidad=>0.90,costo=>8.0,recompensa=>100.0],
+				[nombre=>entregar,probabilidad=>0.99,costo=>5.0,recompensa=>300.0]
+			]
+		],
+		[ubicacion=>o6]
+	],
+	[
+		id=>o13,
+		id_padre=>c11,
+		[
+			nombre=>sandwich,
+			acciones=>[
+				[nombre=>buscar,probabilidad=>0.80,costo=>5.0,recompensa=>50.0],
+				[nombre=>agarrar,probabilidad=>0.85,costo=>9.0,recompensa=>100.0],
+				[nombre=>entregar,probabilidad=>0.99,costo=>5.0,recompensa=>300.0]
+			]
+		],
+		[ubicacion=>o6]
+	],
+	[
+		id=>o14,
+		id_padre=>c11,
+		[
+			nombre=>refresco,
+			acciones=>[
+				[nombre=>buscar,probabilidad=>0.96,costo=>2.0,recompensa=>50.0],
+				[nombre=>agarrar,probabilidad=>0.95,costo=>2.0,recompensa=>80.0],
+				[nombre=>entregar,probabilidad=>0.99,costo=>3.0,recompensa=>300.0]
+			]
+		],
+		[ubicacion=>o7]
+	],
+	[
+		id=>o15,
+		id_padre=>c11,
+		[
+			nombre=>agua,
+			acciones=>[
+				[nombre=>buscar,probabilidad=>0.70,costo=>15.0,recompensa=>50.0],
+				[nombre=>agarrar,probabilidad=>0.95,costo=>10.0,recompensa=>80.0],
+				[nombre=>entregar,probabilidad=>0.99,costo=>10.0,recompensa=>300.0]
+			]
+		],
+		[ubicacion=>o7]
+	],
+	[
+		id=>o16,
+		id_padre=>c11,
+		[
+			nombre=>cafe,
+			acciones=>[
+				[nombre=>buscar,probabilidad=>0.85,costo=>10.0,recompensa=>50.0],
+				[nombre=>agarrar,probabilidad=>0.70,costo=>20.0,recompensa=>15.0],
+				[nombre=>entregar,probabilidad=>0.85,costo=>20.0,recompensa=>400.0]
+			]
+		],
+		[ubicacion=>o7]
+	]
+].
